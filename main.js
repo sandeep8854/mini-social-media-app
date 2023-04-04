a1049fb6289ec0ae3f14fb49bf197f13929d06b7
Sandeep Prajapati <sandeep.prajapati.23.2@mountblue.tech>
2023-04-04 13:20:04 +0530

    desinging of main functionality

---
+++ main.js
@@ -0,0 +1,50 @@
+let form = document.getElementById("form");
+let input = document.getElementById("input");
+let msg = document.getElementById("msg");
+let posts = document.getElementById("posts");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log("Button Clicked..");
+  formValidations();
+});
+
+let formValidations = () => {
+  if (input.value === "") {
+    msg.innerHTML = "Post can't be blank";
+    console.log("Failure");
+  } else {
+    msg.innerHTML = "";
+    console.log("Successes");
+    acceptData();
+  }
+};
+
+let data = {};
+
+let acceptData = () => {
+  data["input"] = input.value;
+  console.log(data);
+  createPost();
+};
+
+let createPost = () => {
+  posts.innerHTML += `
+  <div>
+            <p>${data.input}</p>
+            <span class="options">
+              <i onClick="editPost(this)"class="fa-solid fa-pen-to-square"></i>
+              <i onClick="deletePost(this)" class="fa-solid fa-trash-can"></i>
+            </span>
+          </div>
+  `;
+  input.value = "";
+};
+
+let deletePost = (e) => {
+  e.parentElement.parentElement.remove();
+};
+
+let editPost = (e) => {
+  input.value = e.parentElement.previousElementSibling.innerHTML;
+  e.parentElement.parentElement.remove();
+};
